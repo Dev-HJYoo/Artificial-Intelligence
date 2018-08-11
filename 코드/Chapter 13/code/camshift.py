55a9272e088563aeb412ec09bbdbf5374c168027
cocopambag <jhdf1234@naver.com>
2018-08-11 20:57:13 +0900

    2018.8.11

--- 코드/Chapter 13/code/camshift.py
+++ 코드/Chapter 13/code/camshift.py
@@ -2,69 +2,69 @@ import cv2
 import numpy as np
 
 # Define a class to handle object tracking related functionality
-class ObjectTracker(object):
+class ObjectTracker(object): # 기능적으로 관련있는 물체를 tacking하기 위한 클래스
     def __init__(self, scaling_factor=0.5):
         # Initialize the video capture object
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(0) # 웹캠 사용하기
 
         # Capture the frame from the webcam
-        _, self.frame = self.cap.read()
+        _, self.frame = self.cap.read() # 프레임 읽어 오기
 
         # Scaling factor for the captured frame
-        self.scaling_factor = scaling_factor
+        self.scaling_factor = scaling_factor # 크기 설정 
 
-        # Resize the frame
+        # Resize the frame # 크기 재설정
         self.frame = cv2.resize(self.frame, None, 
                 fx=self.scaling_factor, fy=self.scaling_factor, 
                 interpolation=cv2.INTER_AREA)
 
-        # Create a window to display the frame
+        # Create a window to display the frame # 프레임이름 설정
         cv2.namedWindow('Object Tracker')
 
         # Set the mouse callback function to track the mouse
         cv2.setMouseCallback('Object Tracker', self.mouse_event)
 
-        # Initialize variable related to rectangular region selection
+        # Initialize variable related to rectangular region selection # bounding box 초기화
         self.selection = None
 
-        # Initialize variable related to starting position 
+        # Initialize variable related to starting position  # 시작점 초기화
         self.drag_start = None
 
-        # Initialize variable related to the state of tracking 
+        # Initialize variable related to the state of tracking  # 트레킹 상태 초기화
         self.tracking_state = 0
 
     # Define a method to track the mouse events
-    def mouse_event(self, event, x, y, flags, param):
+    def mouse_event(self, event, x, y, flags, param): # 마우스 event에 대한 함수
         # Convert x and y coordinates into 16-bit numpy integers
-        x, y = np.int16([x, y]) 
+        x, y = np.int16([x, y])  # x와 y 좌표를 16비트로 설정
 
         # Check if a mouse button down event has occurred
-        if event == cv2.EVENT_LBUTTONDOWN:
+        if event == cv2.EVENT_LBUTTONDOWN: # 마우스를 클릭할 경우
             self.drag_start = (x, y)
             self.tracking_state = 0
 
         # Check if the user has started selecting the region
-        if self.drag_start:
-            if flags & cv2.EVENT_FLAG_LBUTTON:
+        if self.drag_start: # 물체를 선택한 경우
+            if flags & cv2.EVENT_FLAG_LBUTTON: # 버튼을 클릭함
                 # Extract the dimensions of the frame
-                h, w = self.frame.shape[:2]
+                h, w = self.frame.shape[:2] # 프레임 높이와 넓이 설정
 
                 # Get the initial position
-                xi, yi = self.drag_start
+                xi, yi = self.drag_start # 초기 좌표
 
-                # Get the max and min values
+                # Get the max and min values # 사각형의 최대값과 최소 값 설정.
                 x0, y0 = np.maximum(0, np.minimum([xi, yi], [x, y]))
                 x1, y1 = np.minimum([w, h], np.maximum([xi, yi], [x, y]))
 
-                # Reset the selection variable
+                # Reset the selection variable # 선택 초기화
                 self.selection = None
 
                 # Finalize the rectangular selection
-                if x1-x0 > 0 and y1-y0 > 0:
+                if x1-x0 > 0 and y1-y0 > 0: # 최종적으로 사각형 생성
                     self.selection = (x0, y0, x1, y1)
 
             else:
-                # If the selection is done, start tracking  
+                # If the selection is done, start tracking   # 트레킹 시작
                 self.drag_start = None
                 if self.selection is not None:
                     self.tracking_state = 1
@@ -72,78 +72,78 @@ class ObjectTracker(object):
     # Method to start tracking the object
     def start_tracking(self):
         # Iterate until the user presses the Esc key
-        while True:
+        while True: # Esc키를 누르기 전까지 재생
             # Capture the frame from webcam
-            _, self.frame = self.cap.read()
+            _, self.frame = self.cap.read() # 프레임 가져오기
             
             # Resize the input frame
-            self.frame = cv2.resize(self.frame, None, 
+            self.frame = cv2.resize(self.frame, None,  # 크기 재조성
                     fx=self.scaling_factor, fy=self.scaling_factor, 
                     interpolation=cv2.INTER_AREA)
 
             # Create a copy of the frame
-            vis = self.frame.copy()
+            vis = self.frame.copy() # 프레임 카피
 
-            # Convert the frame to HSV colorspace
+            # Convert the frame to HSV colorspace # HSV 컬러로 변경
             hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
 
-            # Create the mask based on predefined thresholds
+            # Create the mask based on predefined thresholds # thresholds 재설정
             mask = cv2.inRange(hsv, np.array((0., 60., 32.)), 
                         np.array((180., 255., 255.)))
 
             # Check if the user has selected the region
-            if self.selection:
+            if self.selection: # 구역을 설정한 경우
                 # Extract the coordinates of the selected rectangle
-                x0, y0, x1, y1 = self.selection
+                x0, y0, x1, y1 = self.selection # 좌표
 
-                # Extract the tracking window
+                # Extract the tracking window # 트레킹 하는거 보여주기
                 self.track_window = (x0, y0, x1-x0, y1-y0)
 
-                # Extract the regions of interest 
+                # Extract the regions of interest  # 구역의 특징 추출
                 hsv_roi = hsv[y0:y1, x0:x1]
                 mask_roi = mask[y0:y1, x0:x1]
 
-                # Compute the histogram of the region of 
+                # Compute the histogram of the region of  # 막대그래프로 계산하기 
                 # interest in the HSV image using the mask
                 hist = cv2.calcHist( [hsv_roi], [0], mask_roi, 
                         [16], [0, 180] )
 
-                # Normalize and reshape the histogram
+                # Normalize and reshape the histogram # 표준화 및 재형성
                 cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX);
                 self.hist = hist.reshape(-1)
 
-                # Extract the region of interest from the frame
+                # Extract the region of interest from the frame # 프레임에서 선택한 구역 추출
                 vis_roi = vis[y0:y1, x0:x1]
 
-                # Compute the image negative (for display only)
+                # Compute the image negative (for display only) # 이미지 음영 계산하기
                 cv2.bitwise_not(vis_roi, vis_roi)
                 vis[mask == 0] = 0
 
-            # Check if the system in the "tracking" mode
+            # Check if the system in the "tracking" mode # 트레킹 모드일 경우
             if self.tracking_state == 1:
                 # Reset the selection variable
-                self.selection = None
+                self.selection = None # 선택부분은 안한다. 
                 
                 # Compute the histogram back projection
-                hsv_backproj = cv2.calcBackProject([hsv], [0], 
+                hsv_backproj = cv2.calcBackProject([hsv], [0],  # back projection 막대그래프 계산
                         self.hist, [0, 180], 1)
 
                 # Compute bitwise AND between histogram 
-                # backprojection and the mask
+                # backprojection and the mask # bit-and로 계산
                 hsv_backproj &= mask
 
-                # Define termination criteria for the tracker
+                # Define termination criteria for the tracker # 강제 종료 부분
                 term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 
                         10, 1)
 
-                # Apply CAMShift on 'hsv_backproj'
+                # Apply CAMShift on 'hsv_backproj' # hsv_backproj를 적용시킨다.
                 track_box, self.track_window = cv2.CamShift(hsv_backproj, 
                         self.track_window, term_crit)
 
-                # Draw an ellipse around the object
+                # Draw an ellipse around the object # bounding box 설정
                 cv2.ellipse(vis, track_box, (0, 255, 0), 2)
 
-            # Show the output live video
+            # Show the output live video # 영상 출
             cv2.imshow('Object Tracker', vis)
 
             # Stop if the user hits the 'Esc' key
--- 코드/Chapter 13/code/optical_flow.py
+++ 코드/Chapter 13/code/optical_flow.py
@@ -2,82 +2,82 @@ import cv2
 import numpy as np
 
 # Define a function to track the object
-def start_tracking():
+def start_tracking(): # 트레킹 시작 함수
     # Initialize the video capture object
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0) # 웹캠 사용
 
     # Define the scaling factor for the frames
-    scaling_factor = 0.5
+    scaling_factor = 0.5 # 크기 설정
 
     # Number of frames to track
-    num_frames_to_track = 5
+    num_frames_to_track = 5 # 트레킹 하는 프레임 갯수
 
-    # Skipping factor
+    # Skipping factor # 스킵하는 프레임 갯수
     num_frames_jump = 2
 
-    # Initialize variables
+    # Initialize variables # 초기화
     tracking_paths = []
     frame_index = 0
 
-    # Define tracking parameters
+    # Define tracking parameters # 트레킹 파라미터 선언
     tracking_params = dict(winSize  = (11, 11), maxLevel = 2,
             criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 
                 10, 0.03))
 
     # Iterate until the user hits the 'Esc' key
-    while True:
+    while True: # Esc키가 나오기 전까지 실행
         # Capture the current frame
-        _, frame = cap.read()
+        _, frame = cap.read() # 현재 프레임 가져오기
 
-        # Resize the frame
+        # Resize the frame # 크기 재설정
         frame = cv2.resize(frame, None, fx=scaling_factor, 
                 fy=scaling_factor, interpolation=cv2.INTER_AREA)
 
-        # Convert to grayscale
+        # Convert to grayscale # gray색깔로 바꾸기
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Create a copy of the frame
+        # Create a copy of the frame # 현재 프레임 카피
         output_img = frame.copy()
 
-        if len(tracking_paths) > 0:
+        if len(tracking_paths) > 0: # 트레킹 요소가 있다면
             # Get images
-            prev_img, current_img = prev_gray, frame_gray
+            prev_img, current_img = prev_gray, frame_gray # 전 과 현재 프레임 설정
 
-            # Organize the feature points
+            # Organize the feature points # 특징 지점 설정
             feature_points_0 = np.float32([tp[-1] for tp in \
                     tracking_paths]).reshape(-1, 1, 2)
 
-            # Compute optical flow
+            # Compute optical flow # optical flow 계산
             feature_points_1, _, _ = cv2.calcOpticalFlowPyrLK(
                     prev_img, current_img, feature_points_0, 
                     None, **tracking_params)
 
-            # Compute reverse optical flow
+            # Compute reverse optical flow # 역optical flow 계산
             feature_points_0_rev, _, _ = cv2.calcOpticalFlowPyrLK(
                     current_img, prev_img, feature_points_1, 
                     None, **tracking_params)
 
-            # Compute the difference between forward and 
+            # Compute the difference between forward and  # optical flow 와 역optical flow 다른점 계산
             # reverse optical flow
             diff_feature_points = abs(feature_points_0 - \
                     feature_points_0_rev).reshape(-1, 2).max(-1)
 
-            # Extract the good points
+            # Extract the good points # 좋은 좌표 추출
             good_points = diff_feature_points < 1
 
-            # Initialize variable
+            # Initialize variable # 트레킹 길 초기화
             new_tracking_paths = []
 
-            # Iterate through all the good feature points 
+            # Iterate through all the good feature points  # 특징 좌표를 모두 실행
             for tp, (x, y), good_points_flag in zip(tracking_paths, 
                         feature_points_1.reshape(-1, 2), good_points):
                 # If the flag is not true, then continue
-                if not good_points_flag:
+                if not good_points_flag: # flag가 false이면 계속 실행
                     continue
 
                 # Append the X and Y coordinates and check if
                 # its length greater than the threshold
-                tp.append((x, y))
+                tp.append((x, y)) # flag가 true이므로 추가한다. ( 더 좋은 결과가 나왔을 경우 )
                 if len(tp) > num_frames_to_track:
                     del tp[0]
 
@@ -86,38 +86,38 @@ def start_tracking():
                 # Draw a circle around the feature points
                 cv2.circle(output_img, (x, y), 3, (0, 255, 0), -1)
 
-            # Update the tracking paths
+            # Update the tracking paths  # 트레킹 패스 업데이트
             tracking_paths = new_tracking_paths
 
-            # Draw lines
+            # Draw lines # 트레킹 패스로 선 끗기
             cv2.polylines(output_img, [np.int32(tp) for tp in \
                     tracking_paths], False, (0, 150, 0))
 
         # Go into this 'if' condition after skipping the 
         # right number of frames
-        if not frame_index % num_frames_jump:
-            # Create a mask and draw the circles
+        if not frame_index % num_frames_jump: # 트레킹 요소가 없을 경우
+            # Create a mask and draw the circles # 특징 좌표 및 원 생성
             mask = np.zeros_like(frame_gray)
             mask[:] = 255
             for x, y in [np.int32(tp[-1]) for tp in tracking_paths]:
                 cv2.circle(mask, (x, y), 6, 0, -1)
 
-            # Compute good features to track
+            # Compute good features to track # 트레킹 특징 계산
             feature_points = cv2.goodFeaturesToTrack(frame_gray, 
                     mask = mask, maxCorners = 500, qualityLevel = 0.3, 
                     minDistance = 7, blockSize = 7) 
 
             # Check if feature points exist. If so, append them
             # to the tracking paths
-            if feature_points is not None:
+            if feature_points is not None: # 트레킹 패스 추가
                 for x, y in np.float32(feature_points).reshape(-1, 2):
                     tracking_paths.append([(x, y)])
 
-        # Update variables
+        # Update variables # 변수 업데이트
         frame_index += 1
         prev_gray = frame_gray
 
-        # Display output
+        # Display output # 출력
         cv2.imshow('Optical Flow', output_img)
 
         # Check if the user hit the 'Esc' key
